ac3dbe35fbe9b093ae0250e221d336202c83fd11
dazinator <darrell.tunnell@googlemail.com>
2018-02-06 18:20:49 +0000

    Created sample module

---
+++ src/Default.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Default.ascx.cs" Inherits="DnnPackager_Sample.Default" %>
+<h1>I came, I read a blog, I conquered!</h1>
---
+++ src/Default.ascx.cs
@@ -0,0 +1,13 @@
+﻿using DotNetNuke.Entities.Modules;
+using System;
+
+namespace DnnPackager_Sample
+{
+    public partial class Default : PortalModuleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
